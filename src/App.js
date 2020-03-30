9342f7a12099ee656451a14dabf4238004decc74
sergiomata <serchmata@gmail.com>
2020-03-29 19:28:42 -0600

    feat: add global css and badge css styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import Badge from "./components/Badge";
+import "bootstrap/dist/css/bootstrap.css";
+import "../src/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Badge
+      firstName="Serch"
+      lastName="Mata"
+      avatar="https://www.gravatar.com/avatar?d=identicon"
+      jobTitle=" FullStack Developer"
+      twitter="thebeatmo"
+    />
   );
 }
 
---
+++ src/components/Badge.js
@@ -0,0 +1,31 @@
+import React from "react";
+import confLogo from "../images/badge-header.svg";
+import "./styles/Badge.css";
+
+class Badge extends React.Component {
+  render() {
+    const firstName = "Sergio";
+    const lastName = "Mata";
+
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia"></img>
+        </div>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src={avatar} alt="Avatar"></img>
+          <h1>
+            {this.props.firstName} <br /> {this.props.lastName}
+          </h1>
+        </div>
+        <div className="Badge__section-info">
+          <h3> {this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
+        </div>
+        <div className="Badge__footer">#PlatziConf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
