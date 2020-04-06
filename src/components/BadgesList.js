e7adf5facf771e188caa62120617c759165efb9a
sergiomata <serchmata@gmail.com>
2020-04-06 14:13:59 -0500

    feat: add badgeList style

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import confLogo from "../images/badge-header.svg";
-import "./styles/Badge.css";
+import "./styles/BadgeList.css";
+import twitterLogo from "../images/twitter.png";
 
 class BadgeList extends React.Component {
   render() {
@@ -8,10 +9,36 @@ class BadgeList extends React.Component {
       <ul className="list-unstyled">
         {this.props.badges.map(badge => {
           return (
-            <li key={badge.id}>
-              <p>
-                {badge.fistName} {badge.lastName}
-              </p>
+            <li key={badge.id} className="Badge__container">
+              <div className="row">
+                <div className="colum">
+                  <img src={badge.avatarUrl} className="Badge__image" />
+                </div>
+                <div className="colum">
+                  <p className="Badge__name">
+                    {badge.fistName} {badge.lastName}
+                  </p>
+                  <div className="row">
+                    <img
+                      className="Badge__image__twitter"
+                      src={twitterLogo}
+                      alt="twitterLogo"
+                    />
+                    <p className="Badge__text__twitter">@{badge.twitter}</p>
+                  </div>
+                  <p className="Badge__jobTitle">{badge.jobTitle}</p>
+                </div>
+              </div>
+              {/* <div className="Badge__data">
+                <p className="Badge__name">
+                  {badge.fistName} {badge.lastName}
+                </p>
+                <img
+                  className="Badge__image__twitter"
+                  src={twitterLogo}
+                  alt="twitterLogo"
+                />
+              </div> */}
             </li>
           );
         })}
