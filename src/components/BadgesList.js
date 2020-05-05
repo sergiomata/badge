6fc2c8b0a15f13ddc357297e3a8077be541a1ff3
sergiomata <serchmata@gmail.com>
2020-05-05 10:24:25 -0500

    feat: add fake server

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,13 +1,24 @@
 import React from "react";
-import confLogo from "../images/badge-header.svg";
+import { Link } from "react-router-dom";
 import "./styles/BadgeList.css";
 import twitterLogo from "../images/twitter.png";
 
 class BadgeList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create New Badge
+          </Link>
+        </div>
+      );
+    }
+
     return (
       <ul className="list-unstyled">
-        {this.props.badges.map(badge => {
+        {this.props.badges.map((badge) => {
           return (
             <li key={badge.id} className="Badge__container">
               <div className="row">
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,62 +1,89 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import BadgesList from "../components/BadgesList";
+
 import "./styles/Badges.css";
-import Conf_Logo from "../images/badge-header.svg";
-import BadgeList from "../components/BadgesList";
+import confLogo from "../images/badge-header.svg";
+import BadgesList from "../components/BadgesList";
+
+import api from "../api";
 
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        id: "1",
-        fistName: "John",
-        lastName: "Doe",
-        email: "john.doe@example.com",
-        jobTitle: "Developer",
-        twitter: "johnDoe",
-        avatarUrl: "https://www.gravatar.com/avatar?d=identicon"
-      },
-      {
-        id: "2",
-        fistName: "Joaquin",
-        lastName: "Phoenix",
-        email: "joaquin.phoenix@example.com",
-        jobTitle: "Developer",
-        twitter: "joaquinPhoenix",
-        avatarUrl: "https://www.gravatar.com/avatar?d=identicon"
-      }
-    ]
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      error: null,
+      data: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = async () => {
+    this.setState({ lading: true, error: null });
+
+    try {
+      const data = await api.badges.list();
+      this.setState({
+        loading: false,
+        data: data,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error,
+      });
+    }
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log({
+      prevProps: prevProps,
+      prevState: prevState,
+    });
+
+    console.log({
+      props: this.props,
+      state: this.state,
+    });
+  }
+
+  componentWillUnmount() { }
+
   render() {
+    if (this.state.loading) {
+      return "loading...";
+    }
+    if (this.state.error) {
+      return `${this.state.error}`;
+    }
     return (
-      <div>
+      <React.Fragment>
         <div className="Badges">
           <div className="Badges__hero">
             <div className="Badges__container">
               <img
                 className="Badges_conf-logo"
-                src={Conf_Logo}
-                alt="Conf_Logo"
-              ></img>
+                src={confLogo}
+                alt="Conf Logo"
+              />
             </div>
           </div>
         </div>
+
         <div className="Badges__container">
           <div className="Badges__buttons">
             <Link to="/badges/new" className="btn btn-primary">
-              {" "}
               New Badge
             </Link>
           </div>
-          <div className="Badges__list">
-            <div className="Badges__container">
-              <BadgeList badges={this.state.data} />
-            </div>
-          </div>
+
+          <BadgesList badges={this.state.data} />
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
